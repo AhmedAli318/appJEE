920066bf8e364c353892bccdba49d202c4442773
redboyyyyy <midox318@gmail.com>
2024-11-20 10:29:53 +0100

    commit

---
+++ appJEE/src/main/java/org/example/appjee/AppJeeApplication.java
@@ -0,0 +1,13 @@
+package org.example.appjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppJeeApplication.class, args);
+    }
+
+}
---
+++ appJEE/src/test/java/org/example/appjee/AppJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.appjee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
